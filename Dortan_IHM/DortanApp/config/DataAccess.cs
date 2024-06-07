c06130488456ae450412afb33270e67b77aedd28
Melih CETINKAYA <cetinkam@iut-acy.local>
2024-06-07 08:14:36 +0200

    on ignore plus dataAccess hassoul

---
+++ Dortan_IHM/DortanApp/config/DataAccess.cs
@@ -0,0 +1,99 @@
+﻿using Npgsql;
+using System.Data;
+using System.Windows;
+
+namespace DortanApp.config
+{
+    public class DataAccess
+    {
+        private static DataAccess instance;
+        private static string strConnexion;
+
+        private DataAccess()
+        {
+        }
+
+        public static DataAccess Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DataAccess();
+                }
+
+                return instance;
+            }
+        }
+
+        public NpgsqlConnection? Connexion
+        {
+            get;
+            set;
+        }
+
+        public void ConnexionBD(string id, string mdp)
+        {
+            strConnexion = $"Server=srv-peda-new;" +
+               $"Port=5433;" +
+               $"Database=dortan;" +
+               $"Search Path=dortan;" +
+               $"User Id={id};" +
+               $"Password={mdp};";
+
+            try
+            {
+                Connexion = new NpgsqlConnection();
+                Connexion.ConnectionString = strConnexion;
+                Connexion.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR : Impossinble de se connecter" + ex, "PAS NORMAL", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void DeconnexionBD()
+        {
+            try
+            {
+                Connexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("ERROR : Impossible de se déconnecter" + ex, "PAS NORMAL JE CROIS", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public DataTable GetData(string selectSQL)
+        {
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(selectSQL, Connexion);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("pb avec : " + selectSQL + e.ToString());
+                return null;
+            }
+        }
+
+        public int SetData(string setSQL)
+        {
+            try
+            {
+                NpgsqlCommand sqlCommand = new NpgsqlCommand(setSQL, Connexion);
+                int nbLines = sqlCommand.ExecuteNonQuery();
+                return nbLines;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("pb avec : " + setSQL + e.ToString());
+                return 0;
+            }
+        }
+    }
+}
